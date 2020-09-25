46265e99eef915a0e2136ccd358e77e34a98559b
Alex <alexandrebdiaz@gmail.com>
2020-09-25 02:23:14 -0700

    JS improvements
    Add modal
    Add movie trailers

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const searchElement = document.querySelector('#search')
 const inputElement = document.querySelector('#inputValue')
 const movieSearch = document.querySelector('#searchResult')
+const modal = document.querySelector('#test')
 
 const endpoint = 'https://api.themoviedb.org/3/search/movie/?api_key=61b3b3cf371ec07e8bfd52975b1b5106'
 const imgURL = 'https://image.tmdb.org/t/p/w500'
@@ -15,7 +16,8 @@ function renderSearch(data){
 searchElement.onclick = function(event){
     event.preventDefault()
     const input = inputElement.value
-    const url = endpoint + '&query=' + input
+    const path = '/search/movie'
+    const url = newURL(path) + '&query=' + input
     
     fetch(url)
         .then((res) => res.json())
@@ -26,24 +28,26 @@ searchElement.onclick = function(event){
     inputElement.value = '' //clean previous search
 }
 
+function newURL (path){
+    const url = `https://api.themoviedb.org/3${path}?api_key=61b3b3cf371ec07e8bfd52975b1b5106&language=en-US`
+    return url
+}
+
+
 function movieSection(movies){
     return movies.map((movie) => {
         // To avoid display null poster img
         if (movie.poster_path){
-            console.log(movie.id)
             return `
             <div class='img-box'>
                 <img src=${imgURL + movie.poster_path}  data-movie-id=${movie.id}/>
-                <div data-toggle="modal" data-target="#modalYT" class='transparent-box'>
-                    <h1 id='movieTitle'>${movie.title}</h1>
-                    <p id='userScore'>User Score: ${movie.vote_average}</p>
-                    <p id='movieID'>Movie ID: ${movie.id}</p>
+                <div data-toggle="modal" data-target="#modalYT" data-movie-id=${movie.id} id='test' class='transparent-box'>
+                    <h1 id='movieTitle' data-movie-id=${movie.id}>${movie.title}</h1>
+                    <p id='userScore' data-movie-id=${movie.id}>User Score: ${movie.vote_average}</p>
                 </div>
             </div>
             
             `
-        
-        
         }
     })
 }
@@ -65,15 +69,40 @@ function movieContainer(movies) {
     return movieElement
 }
 
-// event delegation
+// Event Delegation
 document.onclick = function(event){
-    const target = event.target;
+    const target = event.target
 
-    if(target.id === 'btn'){
-        console.log('hi')
+
+
+
+    if(target.id === 'test' ||target.id === 'movieTitle' ||target.id === 'userScore'){
+        const movieID = target.dataset.movieId
+        const path = `/movie/${movieID}/videos`
+        const videoURL = newURL(path)
+
+        fetch(videoURL)
+            .then((res) => res.json())
+            .then((data) => {
+                const video = data.results[0].key
+                console.log(video)
+                const newIframe = createIframe(video)
+                console.log(newIframe)
+                modal.appendChild(newIframe)
+            })
+            .catch((error) => console.log(error))
+        
     }
-
+    
 }
 
+function createIframe(video){
+    const iframe = document.createElement('iframe')
+    iframe.src = `https://www.youtube.com/embed/${video}`
+    iframe.className = `embed-responsive-item`
+    iframe.allowFullscreen = true
+    return iframe
+}
 
-// https://api.themoviedb.org/3/movie/49453/videos?api_key=61b3b3cf371ec07e8bfd52975b1b5106&language=en-US
+// 1:11:20
+// 53:23
